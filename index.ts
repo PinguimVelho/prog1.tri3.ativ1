df66eb064df077c11ec8bfe1b4c688e186363158
Your Name <you@example.com>
2026-09-02 11:47:13 -0300

    a

--- index.ts
+++ index.ts
@@ -2,9 +2,19 @@ const srv = Bun.serve({
     port: 1337,
     routes: {
         "/teste": {
-            GET: () => new Response('Francisco GET'),
+            GET: async (req) {
+                const url = new URL(requestAnimationFrame.url)
+                const search = url.searchParams
+                const nome = search.get('nome')
+                console.log(nome);
+                return new Response('Francisco GET')
+            },
             PUT: () => new Response('Francisco PUT'),
-            POST: () => new Response('Francisco POST'),
+            POST: async (req) {
+                const body = await req.body.text()
+                console.log(body);
+                return new Response('Francisco POST')  
+            },
             DELETE: () => new Response('Francisco DELETE')
         }
     }
